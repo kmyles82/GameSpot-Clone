6b73b69f112898744b166a114edbf2786a39c79c
kmyles82 <kmyles82@gmail.com>
2019-12-14 11:22:55 -0600

    installed wysiwyg, added routes for posts, create dashboard components

--- src/components/Dashboard/addPost.vue
+++ src/components/Dashboard/addPost.vue
@@ -1,15 +1,82 @@
 <template>
-    <div>
-        add posts
-    </div>
+  <div class="dashboard_form">
+    <h1>Add posts</h1>
+    <form @submit.prevent="submitHandler">
+      <div class="input_field" :class="{ invalid: $v.formdata.title.$error }">
+        <label>Title</label>
+        <input
+          type="text"
+          v-model="formdata.title"
+          @blur="$v.formdata.title.$touch()"
+        />
+        <p class="error_label" v-if="$v.formdata.title.$error">
+          This input is required
+        </p>
+      </div>
+
+      <div class="input_field" :class="{ invalid: $v.formdata.desc.$error }">
+        <label>Description</label>
+        <input
+          type="text"
+          v-model="formdata.desc"
+          @blur="$v.formdata.desc.$touch()"
+        />
+        <p class="error_label" v-if="$v.formdata.desc.$error">
+          This input is required
+        </p>
+        <p class="error_label" v-if="!$v.formdata.desc.maxLength">
+          Must not be greater than
+          {{ $v.formdata.desc.$params.maxLength.max }} characters
+        </p>
+      </div>
+
+      <div class="input_field">
+        <wysiwyg/>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+import { required, maxLength } from "vuelidate/lib/validators";
+import wysiwyg from "vue-wysiwyg";
+
+export default {
+  data() {
+    return {
+      formdata: {
+        title: "",
+        desc: "",
+        content: "",
+        rating: ""
+      }
+    };
+  },
+  validations: {
+    formdata: {
+      title: {
+        required
+      },
+      desc: {
+        required,
+        maxLength: maxLength(5)
+      },
+      rating: {
+        required
+      }
     }
+  },
+  methods: {
+    submitHandler() {}
+  }
+};
 </script>
 
-<style>
+<style scoped>
+@import "~vue-wysiwyg/dist/vueWysiwyg.css";
 
+.input_field.invalid input,
+.input_field.invalid select {
+  border: 1px solid red;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
+/* eslint-disable prettier/prettier */
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/routes";
 import store from "./store";
 import VueCarousel from "vue-carousel";
 import Button from "./components/Utils/button";
 import { MdCard } from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
 import VueResource from "vue-resource";
 import Vuelidate from "vuelidate";
+import wysiwyg from 'vue-wysiwyg'
+
 
 Vue.use(Vuelidate);
-Vue.use(VueResource);
+Vue.use(wysiwyg,{});
 Vue.use(MdCard);
 Vue.use(VueCarousel);
 
+Vue.use(VueResource);
 Vue.http.options.root = "https://gamespot-79a3e.firebaseio.com";
 
 Vue.component("app-button", Button);
